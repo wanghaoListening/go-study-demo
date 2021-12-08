4fc8d24c0e01dde90501ab7a89bb9a41de7c6f04
wanghaoListening <wanghaotime@gmail.com>
2021-12-08 10:32:54 +0800

    反射代码demo

--- demo/reflect_exe.go
+++ demo/reflect_exe.go
@@ -15,6 +15,42 @@ func reflectType(x interface{}) {
 	fmt.Printf("the name is : %v , the kind is : %v\n", v.Name(), v.Kind())
 }
 
+func reflectVaklue(x interface{}) {
+
+	v := reflect.ValueOf(x)
+	//值的种类
+	switch v.Kind() {
+
+	case reflect.Int64:
+		fmt.Println()
+
+	case reflect.Float64:
+		fmt.Println()
+	case reflect.Uint32:
+
+	}
+}
+
+//通过反射设置变量的值
+
+func reflectSetValue1(x interface{}) {
+
+	v := reflect.ValueOf(x)
+
+	if v.Kind() == reflect.Int64 {
+		v.SetInt(300) //修改的是副本
+	}
+}
+
+func reflectSetValue2(x interface{}) {
+
+	v := reflect.ValueOf(x)
+
+	if v.Elem().Kind() == reflect.Int64 {
+		v.Elem().SetInt(300)
+	}
+}
+
 func main() {
 
 	var name string = "john"
@@ -25,4 +61,12 @@ func main() {
 
 	var door = Door{}
 	reflectType(door)
+
+	var temp int64 = 20
+
+	reflectSetValue1(&temp)
+	fmt.Println(temp)
+
+	reflectSetValue2(&temp)
+	fmt.Println(temp)
 }
